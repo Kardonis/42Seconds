1b013a66a8072205977fcdd70e5de927cf0f584f
Kardonis <36998694+Kardonis@users.noreply.github.com>
2020-02-19 22:30:48 -0500

    Revert "refining level 9"
    This reverts commit 25863ac256d2335cad3b72f17154e434fa09bdde.

--- Sci_fi_42_seconds/Source/Sci_fi_42_seconds/Sci_fi_42_seconds.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Sci_fi_42_seconds.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sci_fi_42_seconds, "Sci_fi_42_seconds" );
