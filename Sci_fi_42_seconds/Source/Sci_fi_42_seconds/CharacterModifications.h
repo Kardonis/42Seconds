25863ac256d2335cad3b72f17154e434fa09bdde
Kardonis <36998694+Kardonis@users.noreply.github.com>
2020-02-19 21:56:03 -0500

    refining level 9

---
+++ Sci_fi_42_seconds/Source/Sci_fi_42_seconds/CharacterModifications.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SCI_FI_42_SECONDS_API CharacterModifications
+{
+public:
+	CharacterModifications();
+	~CharacterModifications();
+};
---
+++ Sci_fi_42_seconds/Source/Sci_fi_42_seconds/Sci_fi_42_seconds.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Sci_fi_42_seconds.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sci_fi_42_seconds, "Sci_fi_42_seconds" );
